c0ef7c4b331b0c15a732210f0375056791af8468
David Kimura <dkimura@pivotal.io>
2018-01-05 17:31:59 +0900

    Add shared_ptr placeholder

---
+++ include/libcpp/memory.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace std
+{
+
+class control_block
+{
+private:
+    long _strong_count;
+    long _weak_count;
+};
+
+template <typename T>
+class shared_ptr
+{
+public:
+    shared_ptr(T* ptr)
+        : _ptr(ptr),
+          _count(new control_block())
+    {
+    }
+
+private:
+    T* _ptr;
+    control_block* _count;
+};
+
+}
